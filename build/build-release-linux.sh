abc7441b0640ef164d6ec759d83b04fbc46f6eff
arianpg <programmer.arian@gmail.com>
2026-01-15 23:34:07 +0900

    feat: Add .deb and .rpm package generation for Linux releases, including desktop entry and icon.

--- build/build-release-linux.sh
+++ build/build-release-linux.sh
@@ -3,10 +3,16 @@
 set -e
 docker build -t civiewer-build-linux -f $(pwd)/build/Dockerfile.linux .
 docker run --rm -v $(pwd):/app -w /app civiewer-build-linux /bin/bash -c "cargo build --release && \
+cargo deb && \
+cargo generate-rpm && \
 rm -rf target/release/CIViewer && \
 mkdir -p target/release/CIViewer && \
 cp target/release/civiewer target/release/CIViewer && \
 cp LICENSE target/release/CIViewer && \
 cp ThirdPartyNotices.txt target/release/CIViewer && \
+cp assets/civiewer.desktop target/release/CIViewer && \
+cp assets/civiewer.svg target/release/CIViewer && \
+cp target/debian/*.deb target/release/ && \
+cp target/generate-rpm/*.rpm target/release/ && \
 cd target/release && \
 zip -r CIViewer-linux.zip CIViewer"
